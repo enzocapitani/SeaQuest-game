eb09b0492805beb013abe152a747b3030c3cd439
Enzo <enzocapitanivieira@gmail.com>
2026-06-17 10:02:10 -0300

    Versão 0.02, agora com o player podendo se movimentar e a criação da caixa do jogo

--- main.c
+++ main.c
@@ -5,24 +5,35 @@
 //IMPORTANTE! o ponto 0 do eixo Y começa no topo, não em baixo
 
 //Constantes do buffer
-#define LARGURA 100
-#define ALTURA 20
+#define LARGURA 125
+#define ALTURA 25
 
 //Constantes do mapa
 #define CARACTERE_AGUA ' '
+#define CARACTERE_SUPERFICE '='
+#define CARACTERE_PAREDES '|'
+#define CARACTERE_CHAO '_'
 
 // Constantes do player
 #define ALTURA_PLAYER 2
 #define LARGURA_PLAYER 9
-
+#define VELOCIDADE_X 2
+#define VELOCIDADE_Y 1
 
 /*
-    Enzo Capitani: Sprite inicial do submarino, ta uma bosta
+    Enzo Capitani: Sprites iniciais do submarino, ta uma bosta
 */
-const char *PLAYER_SPRITE[ALTURA_PLAYER] ={
+const char *PLAYER_ESQUERDA[ALTURA_PLAYER] ={
+    "   ++++ +",
+    "==+++++++",
+};
+
+const char *PLAYER_DIREITA[ALTURA_PLAYER] ={
     "+ ++++   ",
     "+++++++==",
 };
+
+const char **PLAYER_SPRITE = PLAYER_DIREITA;
 
 /*
     Enzo Capitani: Struct do player, ainda tem q colocar o nivel de oxigenio,
@@ -77,8 +88,24 @@ void desenha_tela(){
         Enzo Capiani: Esse loop preenche os caracteres da agua e define as cores,
         esse atributte tem q deixar as 3 cores primarias (eu acho que sao), se remover
         um ele cria uma mistura, as cores são em hexadecimal
+        Enzo Capitani: Foi adicionada a criação da "caixa" do mapa
     */
     for(int i = 0; i < LARGURA*ALTURA; i++ ){
+        if(i > LARGURA && i < LARGURA*2){
+            consoleBuffer[i].Char.AsciiChar = CARACTERE_SUPERFICE;
+            consoleBuffer[i].Attributes = FOREGROUND_BLUE ;
+            continue;
+        }
+        if(i % LARGURA == 0 || i % LARGURA == LARGURA-1){
+            consoleBuffer[i].Char.AsciiChar = CARACTERE_PAREDES;
+            consoleBuffer[i].Attributes = FOREGROUND_BLUE;
+            continue;
+        }
+        if(i > LARGURA * ALTURA - LARGURA && i < LARGURA*ALTURA){
+            consoleBuffer[i].Char.AsciiChar = CARACTERE_CHAO;
+            consoleBuffer[i].Attributes = FOREGROUND_BLUE;
+            continue;
+        }
         consoleBuffer[i].Char.AsciiChar = CARACTERE_AGUA;
         consoleBuffer[i].Attributes = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED;
     }
@@ -93,6 +120,7 @@ void desenha_tela(){
             //Essa formula aq
             int indice = (player.y + i) * LARGURA + (player.x + j);
             consoleBuffer[indice].Char.AsciiChar = PLAYER_SPRITE[i][j];
+            consoleBuffer[indice].Attributes = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED;
         }
     }
 
@@ -104,6 +132,45 @@ void desenha_tela(){
 
 }
 
+/*
+    Enzo Capitani: Parte das acoes do player, movimentação e etc, precisa adicionar a ação de atirar
+*/
+void acoesPlayer(){
+    if(GetAsyncKeyState(VK_RIGHT)){
+         player.x+=VELOCIDADE_X;
+         PLAYER_SPRITE = PLAYER_DIREITA;
+    }
+    if(GetAsyncKeyState(VK_LEFT)){
+         player.x-=VELOCIDADE_X;
+         PLAYER_SPRITE = PLAYER_ESQUERDA;
+    }
+    if(GetAsyncKeyState(VK_UP)){
+         player.y-=VELOCIDADE_Y;
+    }
+    if(GetAsyncKeyState(VK_DOWN)){
+         player.y+=VELOCIDADE_Y;
+    }
+}
+
+/*
+    Enzo Capitani: O update() é responsável por atualizar tudo que é necessário nas acoes do jogo
+    atualmente ele só verifica se o player saiu do mapa e se saiu, põe ele de volta
+*/
+void update(){
+    if(player.y < 1){
+        player.y = 1;
+    }
+    if(player.y + ALTURA_PLAYER > ALTURA){
+        player.y = ALTURA - ALTURA_PLAYER;
+    }
+    if(player.x < 1 ){
+        player.x = 1;
+    }
+    if(player.x + LARGURA_PLAYER > LARGURA-1){
+        player.x = LARGURA - LARGURA_PLAYER-1;
+    }
+}
+
 
 int main(){
     //Enzo Capitani: aqui define as posições iniciais do player
@@ -112,12 +179,13 @@ int main(){
 
     // Enzo Capitani: aqui indica a saida padrão do programa, que no caso a saída é o console
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    
     //Game loop
     while(1){
-        player.x++;
-        
+        acoesPlayer();
+        update();
         desenha_tela();
-        Sleep(100);
+        Sleep(90);
     }
 
     return 0;
